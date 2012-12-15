c79c3cbff03f98f269dc5f6476c305c0972fab1f
Mike <mike@iMac-ubuntu.(none)>
2012-12-15 13:26:42 +0100

    added support for sequential computation

---
+++ prefixsums/seq.c
@@ -0,0 +1,12 @@
+#include "seq.h"
+#include "common.h"
+
+void prefix_scan_seq(T *x, long n)
+{
+  long i;
+
+  for(i=1; i<n; ++i)
+    {
+      x[i]+=x[i-1];
+    }
+}
---
+++ prefixsums/seq.h
@@ -0,0 +1,8 @@
+#ifndef __SEQ_H__
+#define __SEQ_H__
+
+#include "common.h"
+
+void prefix_scan_seq(T *x, long n);
+
+#endif
