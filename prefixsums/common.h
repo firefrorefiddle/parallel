e48b8bf9237e511c8d705ca2c8a7aeded3b95a4b
Mike <mike@iMac-ubuntu.(none)>
2012-12-15 10:50:55 +0100

    initial commit

---
+++ prefixsums/common.h
@@ -0,0 +1,6 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+typedef enum { true=1, false=0 } bool;
+
+#endif
---
+++ prefixsums/time.h
@@ -0,0 +1,4 @@
+#include <time.h>
+#include <stdio.h>
+
+void write_timediff(FILE *stream, struct timespec t0, struct timespec t1, long size);
