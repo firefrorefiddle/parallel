547b71010cec30df0b09b4a43f38840381458e76
Mike <mike@iMac-ubuntu.(none)>
2012-12-15 15:21:40 +0100

    correct the nanoseconds time display

--- prefixsums/time.c
+++ prefixsums/time.c
@@ -6,7 +6,7 @@ void write_timediff(FILE *stream, struct timespec t0, struct timespec t1, long s
     (t1.tv_sec * 1000000000 + t1.tv_nsec) -
     (t0.tv_sec * 1000000000 + t0.tv_nsec);
 
-  float ns_per_elem = diff / size;
+  float ns_per_elem = diff * 1.0 / size;
 
-  fprintf(stream, "Elapsed time: %.2f seconds (%.1f ns per element).\n", diff/1000000000.0, ns_per_elem);
+  fprintf(stream, "Elapsed time: %.2f seconds (%.2f ns per element).\n", diff/1000000000.0, ns_per_elem);
 }
