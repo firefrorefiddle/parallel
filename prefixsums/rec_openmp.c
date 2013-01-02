3b80ad44d1c0eec6d472c1a220656dee4362520c
Mike <mikehartl17@gmail.com>
2013-01-02 10:54:31 +0100

    added option for omitting parallism for comparison

--- prefixsums/rec_openmp.c
+++ prefixsums/rec_openmp.c
@@ -11,12 +11,18 @@ void prefix_scan_rec(T *x, long n)
 
   long i;
 
+#ifndef NO_PARALLEL
 #pragma omp parallel shared(y)
   {
 #pragma omp for
+#endif
+
     for (i=0; i<n/2; i++) 
       y[i] = x[2*i] + x[2*i + 1];
+
+#ifndef NO_PARALLEL
   }
+#endif
 
   debug_ar(y, n/2);
 
@@ -24,15 +30,21 @@ void prefix_scan_rec(T *x, long n)
 
   x[1] = y[0];
 
+#ifndef NO_PARALLEL
 #pragma omp parallel private(i) shared(x)
   {
 #pragma omp for
+#endif
+
     for (i=1; i<n/2; i++) 
       {
 	x[2*i] = y[i-1] + x[2*i];
 	x[2*i+1] = y[i];
       }
+
+#ifndef NO_PARALLEL
   }
+#endif
 
   if (odd(n)) 
     x[n-1] = y[n/2-1]+x[n-1];
