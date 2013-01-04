5389fe54a0a7a1c6fb2f01f840e594293afd266c
Mike <mikehartl17@gmail.com>
2013-01-04 10:29:10 +0100

    styling issues

--- prefixsums/nrec_openmp.c
+++ prefixsums/nrec_openmp.c
@@ -5,51 +5,50 @@
 
 void prefix_scan_nrec(T *x, long n)
 {
-	int i, k, kk;
+  int i, k, kk;
 
 
-	for (k=1; k<n; k=kk)
-	{
+  for (k=1; k<n; k=kk)
+    {
 		
-		kk = k<<1; // double
-
-	#ifndef NO_PARALLEL
-	#pragma omp parallel shared(x)
-			{
-	#pragma omp for
-	#endif
-
-
-			for (i=kk-1; i<n; i+=kk)
-			{
-				x[i] = x[i-k]+x[i]; 
-			} 
+      kk = k<<1; // double
 
 #ifndef NO_PARALLEL
-		}
+#pragma omp parallel shared(x)
+      {
+#pragma omp for
 #endif
-	}
 
-	for (k=k>>1; k>1; k=kk)
-	{
-		kk = k>>1; // halve
+	for (i=kk-1; i<n; i+=kk)
+	  {
+	    x[i] = x[i-k]+x[i]; 
+	  } 
 
-	#ifndef NO_PARALLEL
-	#pragma omp parallel shared(x)
-		{
-	#pragma omp for
-	#endif
+#ifndef NO_PARALLEL
+      }
+#endif
+    }
+
+  for (k=k>>1; k>1; k=kk)
+    {
+      kk = k>>1; // halve
+
+#ifndef NO_PARALLEL
+#pragma omp parallel shared(x)
+      {
+#pragma omp for
+#endif
 
 
-			for (i=k-1; i<n-kk; i+=k)
-			{
-				x[i+kk] = x[i]+x[i+kk]; 
-			} 
+	for (i=k-1; i<n-kk; i+=k)
+	  {
+	    x[i+kk] = x[i]+x[i+kk]; 
+	  } 
 		
 #ifndef NO_PARALLEL
-		}
+      }
 #endif
 
-	}
+    }
 
 }
